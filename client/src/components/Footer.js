7146e3b6a7757e1fab6d84b4a6e583658db38d05
Jonathan Medina <jonathanmedhed@gmail.com>
2020-03-07 12:45:50 +0000

    replace facebook link with email

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
   return (
     <footer className='footer py-1 bg-primary'>
       <div>
-        <a href='http://www.facebook.com/jonathan.m.heddrich'>
-          <i class='fab fa-facebook-square fa-2x'></i>
+        <a href='mailto: jonathanmedhed@gmail.com'>
+          <i class='fas fa-envelope-square fa-2x'></i>
         </a>
         <a href='https://www.linkedin.com/in/jonathanmedhed/'>
           <i class='fab fa-linkedin fa-2x'></i>
