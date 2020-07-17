407dace1923a7238cc90a76fc24f88f538f908cf
Jonathan Medina <jonathanmedhed@gmail.com>
2020-07-16 22:16:51 -0400

    added myshop project, remove carshop and listtool

--- client/src/components/Lighbox.js
+++ client/src/components/Lighbox.js
@@ -1,143 +1,25 @@
 import React from 'react';
 
 function Lightbox({ name, toggle, show }) {
-  return (
-    <div className='lightbox'>
-      <div className='times hide-sm'>
-        <i onClick={() => toggle(!show)} className='fas fa-times'></i>
-      </div>
-      <div
-        className={`${
-          name.includes('mobile') ? 'times-y-mobile show-sm' : 'times-y show-sm'
-        }`}
-      >
-        <i onClick={() => toggle(!show)} className='fas fa-times'></i>
-      </div>
-      <div className='lightbox-img'>
-        {name === 'showcase' && <img src={require('../img/showcase.jpg')} />}
-        {name === 'screen-carshop-admin' && (
-          <img src={require('../img/carshop-admin.gif')} />
-        )}
-        {name === 'screen-carshop-media' && (
-          <img src={require('../img/carshop-media.gif')} />
-        )}
-        {name === 'screen-carshop-mobile-sm' && (
-          <img
-            className='img-mobile-sm'
-            src={require('../img/carshop-mobile.gif')}
-          />
-        )}
-        {name === 'screen-carshop-layout' && (
-          <img src={require('../img/carshoplayout.gif')} />
-        )}
-        {name === 'screen-carshop' && (
-          <img src={require('../img/carshop1.gif')} />
-        )}
-        {name === 'screen-easy-create-order' && (
-          <img src={require('../img/easy-create-order.gif')} />
-        )}
-        {name === 'screen-easy-fav' && (
-          <img src={require('../img/easy-fav.gif')} />
-        )}
-        {name === 'screen-easy-main' && (
-          <img src={require('../img/easy-main.gif')} />
-        )}
-        {name === 'screen-easy-mobile-sm' && (
-          <img
-            className='img-mobile-sm'
-            src={require('../img/easy-mobile.gif')}
-          />
-        )}
-        {name === 'screen' && <img src={require('../img/listool.gif')} />}
-        {name === 'screen-listool-mobile-add' && (
-          <img src={require('../img/listtool-add.gif')} />
-        )}
-        {name === 'screen-listool-mobile-add-sm' && (
-          <img
-            className='img-mobile-vertical-thin'
-            src={require('../img/listtool-add.gif')}
-          />
-        )}
-        {name === 'screen-listool-mobile-custom' && (
-          <img src={require('../img/listtool-custom.gif')} />
-        )}
-        {name === 'screen-listool-mobile-custom-sm' && (
-          <img
-            className='img-mobile-vertical-thin'
-            src={require('../img/listtool-custom.gif')}
-          />
-        )}
-        {name === 'screen-listool-mobile-separate' && (
-          <img src={require('../img/listtool-separate.gif')} />
-        )}
-        {name === 'screen-mobile-sm' && (
-          <img
-            className='img-mobile-vertical-thin'
-            src={require('../img/listool.gif')}
-          />
-        )}
-        {name === 'screen-listool-mobile-separate-sm' && (
-          <img
-            className='img-mobile-vertical-thin'
-            src={require('../img/listtool-separate.gif')}
-          />
-        )}
-        {name === 'screen-easy-mobile' && (
-          <img className='img-mobile' src={require('../img/easy-mobile.gif')} />
-        )}
-        {name === 'screen-carshop-mobile' && (
-          <img
-            className='img-mobile'
-            src={require('../img/carshop-mobile.gif')}
-          />
-        )}
-      </div>
-      <div className='lightbox-img-vertical'>
-        {name === 'screen-carshop-admin-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/carshop-admin.gif')}
-          />
-        )}
-        {name === 'screen-carshop-media-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/carshop-media.gif')}
-          />
-        )}
-        {name === 'screen-carshop-layout-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/carshoplayout.gif')}
-          />
-        )}
-        {name === 'screen-carshop-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/carshop1.gif')}
-          />
-        )}
-        {name === 'screen-easy-create-order-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/easy-create-order.gif')}
-          />
-        )}
-        {name === 'screen-easy-fav-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/easy-fav.gif')}
-          />
-        )}
-        {name === 'screen-easy-main-sm' && (
-          <img
-            className='img-mobile-vertical'
-            src={require('../img/easy-main.gif')}
-          />
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="lightbox">
+			<div className="times hide-sm">
+				<i onClick={() => toggle(!show)} className="fas fa-times"></i>
+			</div>
+			<div className={`${name.includes('mobile') ? 'times-y-mobile show-sm' : 'times-y show-sm'}`}>
+				<i onClick={() => toggle(!show)} className="fas fa-times"></i>
+			</div>
+			<div className={`${name.includes('mobile') ? 'lightbox-img-mobile' : 'lightbox-img'}`}>
+				<img src={require(`../img/${name}`)} />
+			</div>
+			{/**
+       
+			<div className="lightbox-img-vertical">
+				<img src={require(`../img/${name}`)} />
+			</div>
+       */}
+		</div>
+	);
 }
 
 export default Lightbox;
--- client/src/resources/Data.js
+++ client/src/resources/Data.js
@@ -1,100 +1,94 @@
 import { Developer, Project, Feature, Technology } from './Objects';
 
 /**
+ * My Online Shop Project Object
+ */
+
+const shopTech = new Technology(['React', 'HTML', 'Css'], ['Node.js'], ['MongoDB'], ['JavaScript']);
+
+const shopfeatCreate = new Feature('Create Online Shop', 'myshop-create.gif', 'myshop-create.gif', [
+	'Simple and quick steps',
+	'Shop layout Customization',
+]);
+
+const shopfeatStatistics = new Feature('Statistics', 'myshop-statistics.gif', 'myshop-statistics.gif', [
+	"Monitor the shop's visits, sales and income",
+]);
+
+const shopfeatOrders = new Feature('Order Management', 'myshop-orders.gif', 'myshop-orders.gif', [
+	'Monitor the orders received',
+]);
+
+export const MyOnlineShop = new Project(
+	'My Online Shop',
+	'Online Shop Creation Website',
+	'https://myonlineshopvzla.herokuapp.com/',
+	'myshop-main.gif',
+	'myshop-main.gif',
+	shopTech,
+	[shopfeatStatistics, shopfeatOrders, shopfeatCreate]
+);
+
+/**
  * Easy Peasy Project Object
  */
 
-const easyTech = new Technology(
-  ['React', 'HTML', 'Css'],
-  ['Node.js'],
-  ['MongoDB'],
-  ['JavaScript']
-);
+const easyTech = new Technology(['React', 'HTML', 'Css'], ['Node.js'], ['MongoDB'], ['JavaScript']);
 
-const easyfeatLayout = new Feature(
-  'Responsive Layout',
-  'screen-easy-mobile',
-  'screen-easy-mobile-sm',
-  ['Layout automatically changes for smaller screens']
-);
+const easyfeatLayout = new Feature('Responsive Layout', 'easy-mobile.gif', 'easy-mobile.gif', [
+	'Layout automatically changes for smaller screens',
+]);
 
-const easyfeatCreate = new Feature(
-  'Useful Tools',
-  'screen-easy-create-order',
-  'screen-easy-create-order-sm',
-  ['Create emails for many suppliers from one products list']
-);
+const easyfeatCreate = new Feature('Useful Tools', 'easy-create-order.gif', 'easy-create-order.gif', [
+	'Create emails for many suppliers from one products list',
+]);
 
-const easyfeatFav = new Feature(
-  'Order Management',
-  'screen-easy-fav',
-  'screen-easy-fav-sm',
-  [
-    'Save orders to favorites for re-use',
-    'Edit orders to remove or add products'
-  ]
-);
+const easyfeatFav = new Feature('Order Management', 'easy-fav.gif', 'easy-fav.gif', [
+	'Save orders to favorites for re-use',
+	'Edit orders to remove or add products',
+]);
 
 export const EasyPeasy = new Project(
-  'EasyPeasy',
-  'Ordering WebApp',
-  'https://easy-peasy-v2.herokuapp.com',
-  'screen-easy-main',
-  'screen-easy-main-sm',
-  easyTech,
-  [easyfeatFav, easyfeatLayout, easyfeatCreate]
+	'EasyPeasy',
+	'Ordering WebApp',
+	'https://easy-peasy-v2.herokuapp.com',
+	'easy-main.gif',
+	'easy-main.gif',
+	easyTech,
+	[easyfeatFav, easyfeatLayout, easyfeatCreate]
 );
 
 /**
  * CarShop Project Object
  */
 
-const carTech = new Technology(
-  ['HTML', 'Css'],
-  ['Django'],
-  ['MongoDB'],
-  ['Python']
-);
+const carTech = new Technology(['HTML', 'Css'], ['Django'], ['MongoDB'], ['Python']);
 
-const carFeatLayout = new Feature(
-  'Beautyful Layout',
-  'screen-carshop-layout',
-  'screen-carshop-layout-sm',
-  [
-    'High definition background pictures for each section',
-    'Carrousel to show highlited listings'
-  ]
-);
+const carFeatLayout = new Feature('Beautyful Layout', 'screen-carshop-layout', 'screen-carshop-layout-sm', [
+	'High definition background pictures for each section',
+	'Carrousel to show highlited listings',
+]);
 
-const carFeatMedia = new Feature(
-  'Media Submition',
-  'screen-carshop-media',
-  'screen-carshop-media-sm',
-  ['Upload pictures for listings']
-);
+const carFeatMedia = new Feature('Media Submition', 'screen-carshop-media', 'screen-carshop-media-sm', [
+	'Upload pictures for listings',
+]);
 
-const carFeatAdmin = new Feature(
-  'Admin Page',
-  'screen-carshop-admin',
-  'screen-carshop-admin-sm',
-  ["Page for Admin to Monitor, Add, Modify, Delete website's data"]
-);
+const carFeatAdmin = new Feature('Admin Page', 'screen-carshop-admin', 'screen-carshop-admin-sm', [
+	"Page for Admin to Monitor, Add, Modify, Delete website's data",
+]);
 
-const carFeatResponsive = new Feature(
-  'Responsive Layout',
-  'screen-carshop-mobile',
-  'screen-carshop-mobile-sm',
-  ['Layout automatically changes for smaller screens']
-);
+const carFeatResponsive = new Feature('Responsive Layout', 'screen-carshop-mobile', 'screen-carshop-mobile-sm', [
+	'Layout automatically changes for smaller screens',
+]);
 
 export const CarShop = new Project(
-  'CarShop',
-  'Vehicle Sale Website',
-  'http://167.99.95.246',
-  'screen-carshop',
-  'screen-carshop-sm',
-  carTech,
-  [carFeatLayout, carFeatMedia, carFeatAdmin, carFeatResponsive]
+	'CarShop',
+	'Vehicle Sale Website',
+	'http://167.99.95.246',
+	'screen-carshop',
+	'screen-carshop-sm',
+	carTech,
+	[carFeatLayout, carFeatMedia, carFeatAdmin, carFeatResponsive]
 );
 
 /**
@@ -104,38 +98,38 @@ export const CarShop = new Project(
 const listTech = new Technology(['Java Swing'], [], [], ['Java']);
 
 const listFeatSeparate = new Feature(
-  "Separate List's Items",
-  'screen-listool-mobile-separate',
-  'screen-listool-mobile-separate-sm',
-  ['Separate items by comma, dot, space, line, etc']
+	"Separate List's Items",
+	'screen-listool-mobile-separate',
+	'screen-listool-mobile-separate-sm',
+	['Separate items by comma, dot, space, line, etc']
 );
 
 const listFeatAdd = new Feature(
-  'Add characters or words',
-  'screen-listool-mobile-add',
-  'screen-listool-mobile-add-sm',
-  [
-    'Sorround with quotes, double quotes etc...',
-    'Add prefixes like exclamation mark, question mark, dash, etc...',
-    'Add suffixes with full stop, comma, hypen, dash...'
-  ]
+	'Add characters or words',
+	'screen-listool-mobile-add',
+	'screen-listool-mobile-add-sm',
+	[
+		'Sorround with quotes, double quotes etc...',
+		'Add prefixes like exclamation mark, question mark, dash, etc...',
+		'Add suffixes with full stop, comma, hypen, dash...',
+	]
 );
 
 const listFeatCustom = new Feature(
-  'Add custom words',
-  'screen-listool-mobile-custom',
-  'screen-listool-mobile-custom-sm',
-  ['Sorround or add prefixes/suffixes such as divs, li, ul, etc...']
+	'Add custom words',
+	'screen-listool-mobile-custom',
+	'screen-listool-mobile-custom-sm',
+	['Sorround or add prefixes/suffixes such as divs, li, ul, etc...']
 );
 
 export const ListTool = new Project(
-  'ListTool',
-  'List Modifier Tool',
-  'https://drive.google.com/file/d/1gUd5igq7RZ3KPKg4J0JkN7v2dSrPLTBV/view?usp=sharing',
-  'screen',
-  'screen-mobile-sm',
-  listTech,
-  [listFeatSeparate, listFeatAdd, listFeatCustom]
+	'ListTool',
+	'List Modifier Tool',
+	'https://drive.google.com/file/d/1gUd5igq7RZ3KPKg4J0JkN7v2dSrPLTBV/view?usp=sharing',
+	'screen',
+	'screen-mobile-sm',
+	listTech,
+	[listFeatSeparate, listFeatAdd, listFeatCustom]
 );
 
 /**
@@ -143,16 +137,16 @@ export const ListTool = new Project(
  */
 
 const devTech = new Technology(
-  ['React', 'HTML', 'Css', 'Bootstrap', 'Sass'],
-  ['Node.js', 'Django'],
-  ['MongoDB', 'PostgreSQL', 'MySQL'],
-  ['JavaScript', 'Java', 'TypeScript', 'Python']
+	['React', 'HTML', 'Css', 'Bootstrap', 'Sass'],
+	['Node.js', 'Django'],
+	['MongoDB', 'PostgreSQL', 'MySQL'],
+	['JavaScript', 'Java', 'TypeScript', 'Python']
 );
 
 export const DevObj = new Developer(
-  'Jonathan Medina',
-  'Bsc Computer Science',
-  'Full Stack Developer(Entry Level)',
-  devTech,
-  [EasyPeasy, CarShop, ListTool]
+	'Jonathan Medina',
+	'Bsc Computer Science',
+	'Full Stack Developer(Entry Level)',
+	devTech,
+	[EasyPeasy, MyOnlineShop]
 );
